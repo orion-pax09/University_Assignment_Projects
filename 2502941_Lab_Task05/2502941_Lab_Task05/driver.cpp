7e7f459edb4ecb5965caeb50fda1ed2256a9ff6c
Muhammad Hamza Khan <Khumza050@gmail.com>
2026-06-06 18:44:11 +0500

    Delete 2502941_Lab_Task05 directory

--- 2502941_Lab_Task05/2502941_Lab_Task05/driver.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Problem01.cpp"
-int main(){
-    int array[] ={3,4,5,6,7,8,9};
-    BIGGERINT obj1(array , 6);
-    BIGGERINT obj2 = obj1;
-    obj1.display();
-    obj2.display();
-}
